03061bcfa0d0b8f737865cc05211ae8344a38517
JavohirSolution <javohirdeveloper12@gmail.com>
2024-01-15 19:31:50 +0500

    os.js,fs.js,path.js created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-    
+import express from 'express';
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on:http://localhost:${PORT}`)
+})
