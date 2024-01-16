612b86687e735e28c9cee7c6d1e952ee5bebc8b5
JavohirSolution <javohirdeveloper12@gmail.com>
2024-01-16 20:55:29 +0500

    learned to work with html page

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req,res) => {
-    console.log(req.url);
+const server = http.createServer((req, res) => {
 
-    res.write("<h2>Salom dunyo</h2>");
-    res.end();
+    if (req.method === 'GET') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'template', 'index.html'), 'utf-8', (err, conten) => {
+                if (err) throw err;
+                res.end(conten);
+            })
+        }
+        else if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'template', 'about.html'), 'utf-8', (err, conten) => {
+                if (err) throw err;
+                res.end(conten);
+            })
+        }
+        else if (req.url === '/contact') {
+            fs.readFile(path.join(__dirname, 'template', 'contact.html'), 'utf-8', (err, conten) => {
+                if (err) throw err;
+                res.end(conten);
+            })
+        }
+    }
+
+    else if (req.method === 'POST') {
+        const body = [];
+
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split("=")[1];
+
+            res.end(`Name successfully added : ${message}`)
+        })
+
+    }
+
 })
 
 server.listen(8000, () => {
