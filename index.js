714960504573d4280a63c79f43a66d46e3bd90f8
JavohirSolution <javohirdeveloper12@gmail.com>
2024-01-16 21:07:16 +0500

    15. Api

--- index.js
+++ index.js
@@ -24,22 +24,30 @@ const server = http.createServer((req, res) => {
                 res.end(conten);
             })
         }
+        else if (req.url === '/api/admin') {
+            res.writeHead(200, { "Content-Type": "text/json" });
+
+            const admin = { name: "Javohir", surename: "To'lqinov", job: "Backend developer" }
+
+            res.end(JSON.stringify(admin));
+
+        }
     }
 
     else if (req.method === 'POST') {
-        const body = [];
+    const body = [];
 
-        req.on('data', data => {
-            body.push(Buffer.from(data));
-        })
+    req.on('data', data => {
+        body.push(Buffer.from(data));
+    })
 
-        req.on('end', () => {
-            const message = body.toString().split("=")[1];
+    req.on('end', () => {
+        const message = body.toString().split("=")[1];
 
-            res.end(`Name successfully added : ${message}`)
-        })
+        res.end(`Name successfully added : ${message}`)
+    })
 
-    }
+}
 
 })
 
